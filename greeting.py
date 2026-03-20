25c85d11b0eeb2fa96eceb6f5454e0dfe1830501
Prajwal dhuli <140580996+Prajwal-88@users.noreply.github.com>
2026-03-20 14:56:10 +0000

    Add Flask app with greeting routes

--- greeting.py
+++ greeting.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from flask import request
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return("Hello World!")
+
+@app.route("/greetme")
+def helloall():
+    name = request.args.get('name')
+    return("Hello {}!".format(name))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
